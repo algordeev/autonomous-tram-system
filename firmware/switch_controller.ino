ecc196535b1c3f090ec804c8b9ab2aebc2ec252d
Aleksandr Gordeev <163018781+algordeev@users.noreply.github.com>
2026-01-05 00:03:03 +0300

    Update switch_controller.ino

--- firmware/switch_controller.ino
+++ firmware/switch_controller.ino
@@ -1,32 +1,32 @@
 #include <Servo.h>
 #include <SPI.h>
-#include <MFRC522.h> // библиотека "RFID".
+#include <MFRC522.h> // RFID library
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-unsigned long uidDec, uidDecTemp;  // для храниения номера метки в десятичном формате
+unsigned long uidDec, uidDecTemp;  // to store tag number in dec format
 Servo servo;
 void setup() {
  //Serial.begin(9600);
-  SPI.begin();  //  инициализация SPI / Init SPI bus.
-  mfrc522.PCD_Init();     // инициализация MFRC522 / Init MFRC522 card.
+  SPI.begin();  // init SPI bus.
+  mfrc522.PCD_Init();     // init MFRC522 card.
   servo.attach(3);
-  servo.write(110);  // устанавливаем серву в закрытое сосотояние
+  servo.write(110);  // set servo in closed state
 }
 
 
 
 void loop() {
-  // Поиск новой метки
+  // new tag search
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
-  // Выбор метки
+  // selecting a tag
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
   uidDec = 0;
-  // Выдача серийного номера метки.
+  // issuing a serial number for the tag
   for (byte i = 0; i < mfrc522.uid.size; i++)
   {
     uidDecTemp = mfrc522.uid.uidByte[i];
@@ -34,24 +34,25 @@ void loop() {
   }
 
 
-//условия
+// conditions
 
 //Serial.println(uidDec);
   
-  if ((uidDec == 2305050007)||(uidDec ==3294433023)) // Сравниваем Uid метки, если он равен заданому то серва открывает.
+  if ((uidDec == 2305050007)||(uidDec ==3294433023)) // we compare the UID of the tag
   {
     
-    servo.write(57); // Поворациваем серву на угол 90 градусов(Отпираем какой либо механизм: задвижку, поворациваем ключ и т.д.)
+    servo.write(57); // if it is equal to the specified one, the servo drive opens the switch
    
   }
 
   
-if ((uidDec == 2464178048)||(uidDec ==1269352972)) // Сравниваем Uid метки, если он равен заданому то серва открывает.
+if ((uidDec == 2464178048)||(uidDec ==1269352972)) //we compare the UID of the tag
   {
     
-    servo.write(127); // Поворациваем серву на угол 90 градусов(Отпираем какой либо механизм: задвижку, поворациваем ключ и т.д.)
+    servo.write(127);  // if it is equal to the specified one, the servo drive closes the switch
    
   }
 
-  //servo.write(0);  // устанавливаем серву в закрытое сосотояние
+  //servo.write(0);  // set servo
 }
+
