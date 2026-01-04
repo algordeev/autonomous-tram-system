4789c64d280d1b08971ea5a01dfcb48cef5aca11
Aleksandr Gordeev <163018781+algordeev@users.noreply.github.com>
2026-01-05 00:05:31 +0300

    Update traffic_lights_controller.ino

--- firmware/traffic_lights_controller.ino
+++ firmware/traffic_lights_controller.ino
@@ -1,7 +1,5 @@
-// присваиваем имена для портов со светодиодами
-
 #include <SPI.h>
-#include <MFRC522.h> // библиотека "RFID".
+#include <MFRC522.h> //  "RFID" library
 #define RED1 5
 #define ORG1 3
 #define GRN1 4
@@ -13,15 +11,15 @@
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-unsigned long uidDec, uidDecTemp;  // для храниения номера метки в десятичном формате
+unsigned long uidDec, uidDecTemp;  // to store tag number in dec format
 
-// задержка между переключением светофора  сек
+// delay in sec between traffic light states
 int pause = 1000;
 
 void setup() {
   //Serial.begin(9600);
-  SPI.begin();  //  инициализация SPI / Init SPI bus.
-  mfrc522.PCD_Init();     // инициализация MFRC522 / Init MFRC522 card.
+  SPI.begin();  //  init SPI bus.
+  mfrc522.PCD_Init();     // init MFRC522 card.
    pinMode(RED1, OUTPUT);
    pinMode(ORG1, OUTPUT);
    pinMode(GRN1, OUTPUT);
@@ -34,7 +32,7 @@ void setup() {
 }
 
 void loop() {
-  // первая фаза
+  // first phase
    digitalWrite(RED1, HIGH);
    digitalWrite(ORG1, LOW);
    digitalWrite(GRN1, LOW);
@@ -43,16 +41,16 @@ void loop() {
    digitalWrite(GRN2, HIGH);
 
 
-// Поиск новой метки
+// new tag search
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
-  // Выбор метки
+  // selecting a tag
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
   uidDec = 0;
-  // Выдача серийного номера метки.
+  // issuing a serial number for the tag
   for (byte i = 0; i < mfrc522.uid.size; i++)
   {
     uidDecTemp = mfrc522.uid.uidByte[i];
@@ -62,7 +60,7 @@ void loop() {
   
   //Serial.print(uidDec);
 if ((uidDec == 2305050007)||(uidDec == 2464178048)||(uidDec ==1269352972)){
-     // вторая фаза
+     // second phase
       digitalWrite(RED1, HIGH);
       digitalWrite(ORG1, HIGH);
       digitalWrite(GRN1, LOW);
@@ -70,7 +68,7 @@ if ((uidDec == 2305050007)||(uidDec == 2464178048)||(uidDec ==1269352972)){
       digitalWrite(ORG2, HIGH);
       digitalWrite(GRN2, LOW);
       delay(pause);
-     // третья фаза
+     // third phase
       digitalWrite(RED1, LOW);
       digitalWrite(ORG1, LOW);
       digitalWrite(GRN1, HIGH);
@@ -78,7 +76,7 @@ if ((uidDec == 2305050007)||(uidDec == 2464178048)||(uidDec ==1269352972)){
       digitalWrite(ORG2, LOW);
       digitalWrite(GRN2, LOW);
       delay(10000);
-     // четвертая фаза
+     // fourth phase
       digitalWrite(RED1, LOW);
       digitalWrite(ORG1, HIGH);
       digitalWrite(GRN1, LOW);
@@ -88,3 +86,4 @@ if ((uidDec == 2305050007)||(uidDec == 2464178048)||(uidDec ==1269352972)){
       delay(pause);
    }
 }
+
